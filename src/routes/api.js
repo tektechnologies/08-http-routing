b73e523b392febb23816be3c1347fa3d8f0b009d
tektechnologies <info@tektechnologies.com>
2018-08-22 20:09:41 -0500

    add json function for get and delete

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,3 +7,17 @@ router.get('/api/notes', (req, res) => {
     { id: 1 },
   ]);
 });
+
+
+router.delete('/api/notes', (req, res) => {
+  json(res, {
+    message: `ID ${req.query.id} was deleted`,
+  });
+});
+
+function json(res, object) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(object));
+  res.end();
+}
