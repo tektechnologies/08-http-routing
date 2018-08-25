612c83bcfeb50db89691b6435ea8b3686bf1c123
tektechnologies <info@tektechnologies.com>
2018-08-25 11:20:35 -0500

    add code clean up

--- src/app.js
+++ src/app.js
@@ -45,53 +45,6 @@ function requestHandler(req,res){
 
   require('./routes/api');
 
-  //   requestParser(req)
-  //     .then(() => {
-  //       if(req.parsedUrl.pathname === '/500'){
-  //         throw new Error('Test Error');
-  //       }
-  //       if(req.method === 'GET' && req.parsedUrl.pathname === '/'){
-  //         html(res, `<!DOCTYPE html>
-  //         <html>
-  //           <head>
-  //             <title> cowsay </title>  
-  //           </head>
-  //           <body>
-  //            <header>
-  //              <nav>
-  //                <ul>
-  //                  <li><a href="/cowsay">cowsay</a></li>
-  //                </ul>
-  //              </nav>
-  //            <header>
-  //            <main>
-  //              <!-- project description -->
-  //            </main>
-  //           </body>
-  //         </html>`);
-  //         return;
-  //       }
-  //       if(req.method === 'GET' && req.parsedUrl.pathname === '/api/cowsay' && req.query.text){
-  //         console.log('Method is post');
-  //         json(res, {
-  //           content: cowsay.say(req.query),
-  //         });
-  //         return;
-  //       }
-  //       if(req.method === 'POST' && req.parsedUrl.pathname === '/api/cowsay'  && req.query.text){
-  //         console.log('Method is POST');
-  //         json(res, {content : cowsay.say({text: req.query.text})});
-       
-  //         return;
-  //       }
-  //       notFound(res);
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //       html(res, err.message, 500, 'Internal Server Error');
-  //     });
-  // }
-
 
   function html(res, content, statusCode = 200, statusMessage = 'OK'){
     res.statusCode = statusCode;
